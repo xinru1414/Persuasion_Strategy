0c2530f9794b152ae43be2cae4425fd6ca5b39a8
Jiaaochen <3150105589@zju.edu.cn>
2019-06-05 12:51:18 +0800

    Delete Config.py

--- Config.py
+++
@@ -1,6 +0,0 @@
-class Config(object):
-    def __init__(self, embedding_size = 128, hidden_size = 256, feature_size = 7, output_size = 1):
-        self.embedding_size = embedding_size
-        self.hidden_size = hidden_size
-        self.feature_size = feature_size
-        self.output_size = output_size
